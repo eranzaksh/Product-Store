167b6a02b67fb8771fecf8fc527370e6784d51c8
Eran Zaksh <eranzaksh@gmail.com>
2023-05-27 14:43:47 +0300

    Test cases before new features

--- test_product.py
+++ test_product.py
@@ -3,7 +3,7 @@ import products
 
 
 def test_right_product():
-    products.Product("An Item", price=1450, quantity=100)
+    assert products.Product("An Item", price=1450, quantity=100)
 
 
 def test_wrong_product_name():
@@ -26,4 +26,13 @@ def test_is_inactive():
     a_product.set_quantity(300)
     assert not a_product.is_active()
 
+
 def test_changes_to_quantity():
+    a_product = products.Product("PC", price=500, quantity=300)
+    a_product.set_quantity(100)
+    assert a_product.get_quantity() == 200
+
+
+def test_buy_larger_quantity_then_exists():
+    a_product = products.Product("PC", price=500, quantity=300)
+    assert a_product.buy(400) == 0
